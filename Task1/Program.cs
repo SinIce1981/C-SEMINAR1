111da390318650930a8a5dbbb18c06a7a1e2ba50
Sinice1981 <vinakubani@rambler.ru>
2022-09-23 12:46:24 +0300

    1 task

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Console.WriteLine("Введите число  " );
+//int a = Convert.ToInt32(Console.ReadLine());
+//int b;
+//b = a*a;
+//Console.WriteLine("Результат: " + b);
+
+
+//int rain = 0;// 0- не идет дождь, 1 - идет дождь
+//if (rain ==1)
+//{
+  //  Console.WriteLine("Возьми зонт");
+//}
+//else
+//{
+  //  Console.WriteLine("можешь идти в сандалях");
+//}
+
+// int time = 11;
+// while(time < 22)
+// {
+//     time++;
+//     Console.WriteLine("Еще можно смотреть мультики, время:" + time);
+// }
+// Console.WriteLine("Пора спать!");
+// Console.WriteLine(time);

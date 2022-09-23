36631a85a1dcd034ed2ed709b38e002e02569987
Sinice1981 <vinakubani@rambler.ru>
2022-09-23 15:05:48 +0300

    add 2 new tasks

---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿//функция возведения в квадрат
+// int Power(int x)
+// {
+
+// return x*x;
+// }
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(Power(num));
+
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int num2 = num* -1;
+while(num2 <= num)
+{
+    Console.WriteLine(num2 +",");
+    num2 = num2+1;
+} 
